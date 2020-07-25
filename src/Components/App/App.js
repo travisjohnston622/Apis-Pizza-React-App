9b5679d9fb7bf89cbfac50d9003853c6fb382161
Travis Johnston <travisjohnston4588@icloud.com>
2020-07-25 18:36:27 -0500

    Routes created in app.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import TypePage from '../Pages/TypePage/TypePage';
+import CustomerPage from '../Pages/CustomerPage/CustomerPage';
+import PizzaPage from '../Pages/PizzaPage/PizzaPage';
+import SummaryPage from '../Pages/SummaryPage/SummaryPage';
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
 				<h1>Apis Pizza</h1>
+				<Router>
+					<Route exact path='/' component={TypePage} />
+					<Route exact path='/Customer' component={CustomerPage} />
+					<Router exact path='/Pizza' component={PizzaPage} />
+					<Router exact path='/Summary' component={SummaryPage} />
+				</Router>
 			</div>
 		);
 	}
